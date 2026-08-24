d5fbbe1c40689248b71aefbc5f244ce47af668ff
Tirth Radadiya <tirthradadiya073@gmail.com>
2026-08-24 16:41:50 +0530

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNet_Header_Footer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
